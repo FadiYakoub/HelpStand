ea6158c881696b956f180cfad9593e810c7cce24
Fadi Yakoub <fadi.yakoub@yh.nackademin.se>
2016-07-14 19:11:42 +0200

    This is first push

---
+++ HelpStand/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelpStand.Startup))]
+namespace HelpStand
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
